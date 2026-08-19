a4a5739d6b5b868d898867b8cdd4c9e54d944f03
sardor <normatovsardor973@gmail.com>
2026-08-19 12:45:59 +0500

    problem3

--- problem3.py
+++ problem3.py
@@ -4,3 +4,5 @@ def rearrange_by_frequency(nums: list[int]) -> list[int]:
     count = Counter(nums)
 
     return sorted(nums, key=lambda x: (-count[x], x))
+
+print(rearrange_by_frequency([4, 5, 6, 5, 4, 3, 4]))
